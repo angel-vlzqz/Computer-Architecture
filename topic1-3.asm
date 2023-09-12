266c4471f66893fd362bbaff0d3ca621494b2095
angel velazquez <21velazquezang@gmail.com>
2023-09-12 15:45:52 -0700

    add system call to print string

--- topic1-3.asm
+++ topic1-3.asm
@@ -8,3 +8,7 @@ main:
     li $v0, 5       #system call to read an integer
     syscall
     move $s0, $v0   #move into register $s0, $v0
+
+    li $v0, 4       #system call to print a string
+    la $a0, input
+    syscall

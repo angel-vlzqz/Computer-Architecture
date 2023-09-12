d1b77698e34c99432db3429e0e4da777be44aa13
angel velazquez <21velazquezang@gmail.com>
2023-09-12 15:44:04 -0700

    allow system call for input and to store integer

--- topic1-3.asm
+++ topic1-3.asm
@@ -5,3 +5,6 @@ input: .space 4     #create variable to store input
 .globl main
 
 main:
+    li $v0, 5       #system call to read an integer
+    syscall
+    move $s0, $v0   #move into register $s0, $v0

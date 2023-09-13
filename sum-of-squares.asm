73f8b359e519b95e099261a5afc5ad3fe2963c44
angel velazquez <21velazquezang@gmail.com>
2023-09-12 23:32:58 -0700

    add remaining arithmetic to sumLoop

--- sum-of-squares.asm
+++ sum-of-squares.asm
@@ -4,6 +4,7 @@
 .data
 n:              .word 10
 sumOfSquares:   .word 0
+
 # ----------------------------------------
 
 .text
@@ -17,7 +18,12 @@ sumLoop:
     mul $t3, $t1, $t1   # index^2 ??
     add $t2, $t2, $t3
 
-# -----
+    add $t1, $t1, 1
+    ble $t1, $t0, sumLoop
+
+    sw  $t2, sumOfSquares
+
+# ----------------------------------------
 #  Done, terminate program.
 li $v0, 10
 syscall # all done!

35bed172243f21ea8d6b3ee1b994c20984ea4b1a
angel velazquez <21velazquezang@gmail.com>
2023-09-12 17:15:57 -0700

    add arithmetic for volume and surfaceArea

--- integer-arithmetic.asm
+++ integer-arithmetic.asm
@@ -1,7 +1,6 @@
 #  This program exemplifies integer arithmetic
 #  To find volume and surface area of a prism
 # ----------------------------------------
-#  Data declarations go in this section.
 .data
 
 aSide:          .word 73    # define side lengths
@@ -11,16 +10,30 @@ cSide:          .word 16
 volume:         .word 0     # define initial volume, surfaceArea
 surfaceArea:    .word 0
 # ----------------------------------------
-#  Program code goes in this section.
+
 .text
 .globl main
 .ent main
 main:
-# -----
-lw $t0, aSide   #load variables into registers
+                    # load variables into registers
+lw $t0, aSide       
 lw $t1, bSide
 lw $t2, cSide
-# -----
+                    # volume = aSide * bSide * cSide
+mul $t3, $t0, $t1   # aSide * bSide
+mul $t4, $t3, $t2   # result * cSide
+
+                    # surfaceArea = 2*(aSide*bSide+aSide*cSide+bSide*cSide)
+mul $t3, $t0, $t1   # a * b
+mul $t4, $t0, $t2   # a * c
+mul $t5, $t1, $t2   # b * c
+
+add $t6, $t3, $t4
+add $t7, $t6, $t5
+
+mul $t7, $t7, 2
+sw  $t7, surfaceArea    # store result into surfaceArea
+
 #  Done, terminate program.
 li $v0, 10
 syscall # all done!

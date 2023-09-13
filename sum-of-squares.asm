1f2e963f3485b5aa92499cbd5225d652a065f750
angel velazquez <21velazquezang@gmail.com>
2023-09-12 23:14:42 -0700

    insert template for sum of squares

---
+++ sum-of-squares.asm
@@ -0,0 +1,18 @@
+#  Sum of Squares test program
+# ----------------------------------------
+
+.data
+
+# ----------------------------------------
+
+.text
+.globl main
+.ent main
+main:
+
+
+# -----
+#  Done, terminate program.
+li $v0, 10
+syscall # all done!
+.end main

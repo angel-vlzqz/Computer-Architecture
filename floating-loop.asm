dd166c938f6b943c9b6fa763c1beac98fcc96ff8
angel velazquez <21velazquezang@gmail.com>
2023-09-14 16:04:07 -0700

    add newLine calls

--- floating-loop.asm
+++ floating-loop.asm
@@ -25,11 +25,27 @@ main:
         s.s $f0, num    # store input in $f0 into num
 
         li $v0, 4       # system call to print string
+        la $a0, newLine # load address to print string
+        syscall         # call OS to print
+
+        li $v0, 4       # system call to print string
         la $a0, print   # load address to be print string
+        syscall         # call OS to print
+
+        li $v0, 4       # system call to print string
+        la $a0, newLine # load address to print string
         syscall         # call OS to print
 
         li $v0, 2       # system call to print float
         l.s $f12, num   # load input into $f12 as float
+        syscall         # call OS to print
+
+        li $v0, 4       # system call to print string
+        la $a0, newLine # load address to print string
+        syscall         # call OS to print
+
+        li $v0, 4       # system call to print string
+        la $a0, newLine # load address to print string
         syscall         # call OS to print
 
         li $v0, 4       # system call to print string
@@ -42,7 +58,8 @@ main:
         li $v0, 4       # system call to print string
         la $a0, goodBye # load string into $a0 as an asciiz
         syscall         # call OS to print
-# -----
+
+# ----------------------------------------
 #  Done, terminate program.
 li $v0, 10
 syscall # all done!

c386db4d95a46bbdc4544a902497cd3c44f8f61c
angel velazquez <21velazquezang@gmail.com>
2023-09-21 15:33:52 -0700

    add base conversion snippet

---
+++ Class Snippets/ConvertBase.asm
@@ -0,0 +1,18 @@
+#  Name and general description of program
+# ----------------------------------------
+
+.data
+
+# ----------------------------------------
+
+.text
+.globl main
+.ent main
+main:
+
+
+# ----------------------------------------
+#  Done, terminate program.
+li $v0, 10
+syscall # all done!
+.end main

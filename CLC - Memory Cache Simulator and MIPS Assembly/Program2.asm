187e9eb071accf1a3c37019f47ad5d7c7707706b
angel velazquez <21velazquezang@gmail.com>
2023-09-29 12:21:18 -0700

    add asciiz variables

--- CLC - Memory Cache Simulator and MIPS Assembly/Program2.asm
+++ CLC - Memory Cache Simulator and MIPS Assembly/Program2.asm
@@ -5,6 +5,10 @@
 .data
 num1: .word 0
 num2: .word 0
+text1:   .asciiz "Enter a number: "
+text2:   .asciiz "Enter another number: "
+text3:   .asciiz "The larger of the two numbers is: "
+newLine: .asciiz "\n"
 
 # ----------------------------------------
 

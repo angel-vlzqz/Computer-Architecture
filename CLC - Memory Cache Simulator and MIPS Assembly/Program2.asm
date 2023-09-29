7accc4a712c260bd6b3f3557b66c679567d4144a
angel velazquez <21velazquezang@gmail.com>
2023-09-29 13:45:58 -0700

    add print statement, update variables, remove newline calls

--- CLC - Memory Cache Simulator and MIPS Assembly/Program2.asm
+++ CLC - Memory Cache Simulator and MIPS Assembly/Program2.asm
@@ -3,12 +3,9 @@
 # ----------------------------------------
 
 .data
-num1: .word 0
-num2: .word 0
 text1:   .asciiz "Enter a number: "
 text2:   .asciiz "Enter another number: "
 text3:   .asciiz "The larger of the two numbers is: "
-newLine: .asciiz "\n"
 
 # ----------------------------------------
 
@@ -24,9 +21,6 @@ main:
     syscall         # call OS to await input
     move $t0, $v0   # move input into $t0
 
-    li $v0, 4       # system call to print string
-    la $a0, newLine # load address
-    syscall         # call OS to print
 
     li $v0, 4       # system call to print string
     la $a0, text2   # load address
@@ -34,7 +28,12 @@ main:
 
     li $v0, 5       # system call to read integer
     syscall         # call OS to await input
-    move $t0, $v0   # move input into $t0
+    move $t1, $v0   # move input into $t0
+
+    li $v0, 4       # system call to print string
+    la $a0, text3   # load address
+    syscall         # call OS to print
+
 
 # ----------------------------------------
 #  Done, terminate program.

d5aa8c9d47739e2b54546dd168eaa4df73829d88
angel velazquez <21velazquezang@gmail.com>
2023-09-29 12:43:38 -0700

    add system call to read integer

--- CLC - Memory Cache Simulator and MIPS Assembly/Program2.asm
+++ CLC - Memory Cache Simulator and MIPS Assembly/Program2.asm
@@ -20,6 +20,9 @@ main:
     la $a0, text1   # load address
     syscall         # call OS to print
 
+    li $v0, 5       # system call to read integer
+    syscall         # call OS to await input
+    move $t0, $v0   # move input into $t0
 
 # ----------------------------------------
 #  Done, terminate program.

c700d79da0bfe4a76bc072dfb51c08adcdf9445d
angel velazquez <21velazquezang@gmail.com>
2023-09-29 12:12:57 -0700

    add template to program 2

---
+++ CLC - Memory Cache Simulator and MIPS Assembly/Program2.asm
@@ -0,0 +1,19 @@
+#  Prints the larger of two numbers specified
+#  at runtime by the user
+# ----------------------------------------
+
+.data
+
+# ----------------------------------------
+
+.text
+.globl main
+.ent main
+main:
+
+
+# ----------------------------------------
+#  Done, terminate program.
+li $v0, 10
+syscall # all done!
+.end main

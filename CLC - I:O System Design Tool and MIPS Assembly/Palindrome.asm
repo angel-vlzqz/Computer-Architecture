9c32b7d8739094cce6460bfaee2428383fe81923
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2023-10-08 23:46:30 -0700

    add new clc folder

--- CLC - I:O System Design Tool and MIPS Assembly/Palindrome.asm
+++ CLC - I:O System Design Tool and MIPS Assembly/Palindrome.asm
@@ -12,7 +12,13 @@
 .globl main
 .ent main
 main:
+    li $v0, 4       # system call to print string
+    la $a0, text1   # load address
+    syscall         # call OS to print
 
+    li $v0, 4       # system call to print string
+    la $a0, text2   # load address
+    syscall         # call OS to print
 
 #-------------------------------------------------
 #  Done, terminate program.
@@ -23,9 +29,8 @@ syscall # all done!
 
 .data
     text1: .asciiz "This program will ask you for a word. 
-                    If your word is a palindrome, I will tell you. 
-                    If not, I will tell you."
-    text2: .asciiz "Enter a word"
+If your word is a palindrome, I will tell you. If not, I will tell you.\n"
+    text2: .asciiz "Enter a word: "
     text3: .asciiz " is a palindrome."
     text4: .asciiz " is NOT a palindrome."
     text5: .asciiz "Would you like to continue? Enter y/n: "

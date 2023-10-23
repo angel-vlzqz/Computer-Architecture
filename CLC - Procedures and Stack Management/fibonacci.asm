d2e54dfdf43345101a1b1b044acbda698bd673d7
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2023-10-22 18:36:33 -0700

    rename recursion file

---
+++ CLC - Procedures and Stack Management/fibonacci.asm
@@ -0,0 +1,23 @@
+#  fibonacci.asm
+#-------------------------------------------------
+#  Author: Angel Velazquez, Alex Utzerath
+#  Creation date: 2023Oct19
+#-------------------------------------------------
+#  This program utilizes reccursion using the
+#  jal and jr procedures
+#-------------------------------------------------
+
+.text
+.globl main
+.ent main
+main:
+
+
+#-------------------------------------------------
+#  Done, terminate program.
+li $v0, 10
+syscall # all done!
+.end main
+#-------------------------------------------------
+
+.data
